868fc62d3a8b657fb00d3c043dd2b6c19370fa8e
Thuy <thuy0867090536@gmail.com>
2025-04-01 18:39:14 +0700

    Tạo ra trang Chi Tiết Tour

--- controllers/clients/tour.controller.js
+++ controllers/clients/tour.controller.js
@@ -13,3 +13,12 @@ module.exports.list = async (req, res) => {
 }
 // list là hàm
 
+module.exports.detail = async (req, res) => {
+
+    const tourList = await Tour.find({});
+
+    res.render("client/pages/tours-detail", {
+        title: "Chi tiết tours"
+    })
+}
+
--- router/client/tour.router.js
+++ router/client/tour.router.js
@@ -3,5 +3,7 @@ const router =require ('express').Router();
 const tourController = require("../../controllers/clients/tour.controller");
 
 router.get('/', tourController.list);
+router.get('/detail', tourController.detail);
+
 
 module.exports = router;
