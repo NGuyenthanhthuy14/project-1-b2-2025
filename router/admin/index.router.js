f5af989f38fc62e58f5201f7fe2c5f5366ededfc
Thuy <thuy0867090536@gmail.com>
2025-04-02 17:52:04 +0700

    1.5 + 1.6

--- router/admin/index.router.js
+++ router/admin/index.router.js
@@ -1,9 +1,12 @@
 const router =require ('express').Router();
 const accountRouters =require ("./account.router")
 const dashboardRouters = require ("./dashboard.router")
+const categoryRouters = require ("./catagory.router")
 
 router.use ('/account', accountRouters)
 
 router.use ('/dashboard', dashboardRouters)
+
+router.use ('/category', categoryRouters)
 
 module.exports = router;
