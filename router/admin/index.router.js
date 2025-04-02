1d4221b88395a313203c6de1545990e3480a7400
Thuy <thuy0867090536@gmail.com>
2025-04-02 17:30:19 +0700

    1.4 Tích hợp giao diện tổng quan

--- router/admin/index.router.js
+++ router/admin/index.router.js
@@ -1,6 +1,9 @@
 const router =require ('express').Router();
 const accountRouters =require ("./account.router")
+const dashboardRouters = require ("./dashboard.router")
 
 router.use ('/account', accountRouters)
+
+router.use ('/dashboard', dashboardRouters)
 
 module.exports = router;
