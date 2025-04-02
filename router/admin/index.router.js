6fe0d216a46547ee7ebb725708202dde492e7861
Thuy <thuy0867090536@gmail.com>
2025-04-02 22:23:32 +0700

    1.12 + 1.13

--- router/admin/index.router.js
+++ router/admin/index.router.js
@@ -4,6 +4,8 @@ const dashboardRouters = require ("./dashboard.router")
 const categoryRouters = require ("./catagory.router")
 const tourRouter = require("./tour.router")
 const orderRouter = require("./order.router")
+const userRouters = require("./user.router")
+const contactRouters = require("./contact.router")
 
 router.use ('/account', accountRouters)
 
@@ -14,5 +16,9 @@ router.use ('/category', categoryRouters)
 router.use ('/tour', tourRouter)
 
 router.use ('/order', orderRouter)
+
+router.use ('/user', userRouters)
+
+router.use ('/contact', contactRouters)
 
 module.exports = router;
