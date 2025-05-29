add4b58b7f422484b2c7002334ce5d01b61dde26
nguyen thanh thuy <nguyenthanhthuy@nguyens-MacBook-Air.local>
2025-05-29 23:39:12 +0700

    03. Làm trang chỉnh sửa

--- controllers/admin/category.controller.js
+++ controllers/admin/category.controller.js
@@ -75,3 +75,104 @@ module.exports.createPost = async (req, res) => {
         code: "success"
     })
 }
+
+module.exports.list = async (req, res) => {
+    const categoryList = await Category.find({
+        deleted: false
+    }).sort({
+        position: "asc" // sắp xếp theo vị trí tăng đân
+    })
+
+    for (const item of categoryList) {
+        if (item.createdBy) {
+            const infoAccountCreated = await AcconutAdmin.findOne ({
+                _id: item.createdBy
+            })
+            item.createdByFullName = infoAccountCreated.fullName
+        }
+    
+        if (item.updatedBy) {
+            const infoAccountupdated= await AcconutAdmin.findOne ({
+                _id: item.updatedBy
+            })
+            item.updatedByFullName = infoAccountupdated.fullName
+        }
+
+        item.createAtFormat = moment(item.createdAt).format("HH:mm - DD/MM/YYYY")
+        item.updateAtFormat = moment(item.createdAt).format("HH:mm - DD/MM/YYYY")
+        
+    }
+
+    res.render("admin/pages/category-list", {
+        title: "Quản lý danh mục",
+        categoryList: categoryList
+    })
+}
+
+module.exports.edit = async (req, res) => {
+    try {
+        const categoryList = await Category.find({
+        deleted: false
+    })
+
+    const categoryTree = categoryHelper.builCategoryTree (categoryList)
+
+    const id = req.params.id
+    const categoryDetail = await Category.findOne({
+        _id: id,
+        deleted: false
+    })
+
+    res.render("admin/pages/category-edit", {
+        title: "Chỉnh sửa danh mục",
+        categoryList: categoryTree,
+        categoryDetail: categoryDetail
+    })
+    } catch (error) {
+        res.redirect(`/${pathAdmin}/category/list`)
+    }
+    
+}
+
+module.exports.editPatch = async (req, res) => {
+
+    try {
+        const id = req.params.id
+
+        if (req.body.position) {
+            req.body.position = parseInt(req.body.position)
+        }
+        else {
+            const totalRecord = await Category.countDocuments({})
+            req.body.position = totalRecord + 1
+        }
+
+        req.body.createdBy = req.account.id;
+        req.body.updatedBy = req.account.id;
+        if (req.file) {
+            req.body.avatar = req.file.path
+        } else {
+            delete req.body.avatar
+        }
+        // req.body.avatar = req.file ? req.file.path : "";
+
+        await Category.updateOne ({
+            _id: id, // Timd bản ghi theo các tiêu chí này
+            deleted: false
+        }, req.body) // chứa trường dữ liệu muốn cập nhật
+
+        // console.log(req.file)
+
+        req.flash ("success", "Cập nhật danh mục thành công")
+        res.json ({
+            code: "success"
+        })
+    } catch (error) {
+        res.json ({
+            code: "error",
+            message: "ID không hợp lệ"
+        })
+    }
+    
+}
+
